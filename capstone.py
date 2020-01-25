4e585856295dfb7c2d5f9278ed3276401ccc78e5
Fatehraj <54749930+Fatehraj@users.noreply.github.com>
2020-01-25 22:07:25 +0530

    Add files via upload

---
+++ capstone.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#this is assignemt for applied data science Capstone and print "hello world"
+
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
